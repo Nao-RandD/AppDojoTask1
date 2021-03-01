73cdaf9eb50118e79efb2cb5fbda8f670346583a
Nao-RandD <nao.r.and.d@gmail.com>
2021-03-02 06:21:11 +0900

    Initial Commit

---
+++ Shared/AppDojoTask1App.swift
@@ -0,0 +1,17 @@
+//
+//  AppDojoTask1App.swift
+//  Shared
+//
+//  Created by Naoyuki Kan on 2021/03/02.
+//
+
+import SwiftUI
+
+@main
+struct AppDojoTask1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
