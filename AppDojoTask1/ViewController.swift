8205fb616c9cb7e600520213a5c016c0df61a38f
Nao-RandD <nao.r.and.d@gmail.com>
2021-03-03 06:43:32 +0900

    [add] Add TextView and Button.

--- AppDojoTask1/ViewController.swift
+++ AppDojoTask1/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var editTextView1: UITextField!
+    @IBOutlet weak var editTextView2: UITextField!
+    @IBOutlet weak var editTextView3: UITextField!
+    @IBOutlet weak var editTextView4: UITextField!
+    @IBOutlet weak var editTextView5: UITextField!
 
+
+    @IBOutlet weak var calButton: UIButton!
+    @IBOutlet weak var resultText: UILabel!
+    
+    
+    @IBAction func tapButton(_ sender: Any) {
+        let field1 = editTextView1.text ?? "0"
+        let field2 = editTextView2.text ?? "0"
+        let field3 = editTextView3.text ?? "0"
+        let field4 = editTextView4.text ?? "0"
+        let field5 = editTextView5.text ?? "0"
+        
+        let sum =  Int(field1)! + Int(field2)! + Int(field3)! + Int(field4)! + Int(field5)!
+        
+        resultText.text = String(sum)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
