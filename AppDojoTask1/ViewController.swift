9fb549ccc86b470a7da7a97c5f1bb794710f4f60
Nao-RandD <nao.r.and.d@gmail.com>
2021-03-03 08:12:00 +0900

    [fix] Fix the problem of calculating text fields even when they do not contain any characters.

--- AppDojoTask1/ViewController.swift
+++ AppDojoTask1/ViewController.swift
@@ -8,35 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var editTextView1: UITextField!
-    @IBOutlet weak var editTextView2: UITextField!
-    @IBOutlet weak var editTextView3: UITextField!
-    @IBOutlet weak var editTextView4: UITextField!
-    @IBOutlet weak var editTextView5: UITextField!
+    @IBOutlet private weak var editTextView1: UITextField!
+    @IBOutlet private weak var editTextView2: UITextField!
+    @IBOutlet private weak var editTextView3: UITextField!
+    @IBOutlet private weak var editTextView4: UITextField!
+    @IBOutlet private weak var editTextView5: UITextField!
 
-
-    @IBOutlet weak var calButton: UIButton!
-    @IBOutlet weak var resultText: UILabel!
-    
-    
-    @IBAction func tapButton(_ sender: Any) {
-        let field1 = editTextView1.text ?? "0"
-        let field2 = editTextView2.text ?? "0"
-        let field3 = editTextView3.text ?? "0"
-        let field4 = editTextView4.text ?? "0"
-        let field5 = editTextView5.text ?? "0"
-        
-        let sum =  Int(field1)! + Int(field2)! + Int(field3)! + Int(field4)! + Int(field5)!
-        
-        resultText.text = String(sum)
-    }
+    @IBOutlet private weak var calButton: UIButton!
+    @IBOutlet private weak var resultText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
     }
-
-
+    
+    @IBAction func tapButton(_ sender: Any) {
+        let field1 = Int(editTextView1.text!) ?? 0
+        let field2 = Int(editTextView2.text!) ?? 0
+        let field3 = Int(editTextView3.text!) ?? 0
+        let field4 = Int(editTextView4.text!) ?? 0
+        let field5 = Int(editTextView5.text!) ?? 0
+        
+        let sum =  field1 + field2 + field3 + field4 + field5
+        
+        resultText.text = String(sum)
+    }
 }
-
